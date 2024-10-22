23094bda6d84f97844d78f307c47c41458a86686
Ahmad_Naufal2 <ans.education.2004@gmail.com>
2024-10-22 02:25:20 +0000

    Update terbaru 22 Oktober 2024

---
+++ lib/home.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/material.dart';
+
+class Myhome extends StatelessWidget {
+  const Myhome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("AbuNof Restaurant"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+              ListTile(
+                title: Text("Ayam Taliwang"),
+                subtitle: Text("22 Oktober 2024"),
+                leading: Image(
+                    image: NetworkImage(
+                        "https://tse4.mm.bing.net/th?id=OIP.7K5z80W2OjjkETP7GEzpEQHaEo&pid=Api&P=0&h=180")),
+              ),
+            ],
+          ),
+        ));
+  }
+}
